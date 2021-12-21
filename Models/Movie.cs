bdbd047b2b5a823008ce945458e407c5694d7e0a
WickyLim <wickylim@virtuosity-global.com>
2021-12-21 11:38:12 +0800

    Add fields, pre-database rebuild

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,8 +12,9 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
-        
+
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string? Rating {  get; set; }
     }
 }
